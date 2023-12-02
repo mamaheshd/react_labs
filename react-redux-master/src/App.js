f1af83f4446306b22e2d9fa4d45d04085187f701
mamaheshd <mamaheshd@gmail.com>
2023-12-02 19:10:57 +0545

    trying

--- react-redux-master/src/App.js
+++ react-redux-master/src/App.js
@@ -1,3 +1,16 @@
+// import React from 'react';
+// import MainPanel from './components/MainPanel';
+
+// function App() {
+//   return (
+//       <div>
+//         <MainPanel/>
+//       </div>
+//     );
+// }
+
+// export default App;
+
 import React from 'react';
 import MainPanel from './components/MainPanel';
 
